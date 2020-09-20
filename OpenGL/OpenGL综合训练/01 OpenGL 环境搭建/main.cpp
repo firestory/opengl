0e0b5f35f3448768e9a4b62eb881f9ffb3abaab9
NiiLove <firestory@sina.com>
2020-09-20 17:02:45 +0800

    综合练习

--- OpenGL/OpenGL综合训练/01 OpenGL 环境搭建/main.cpp
+++ OpenGL/OpenGL综合训练/01 OpenGL 环境搭建/main.cpp
@@ -22,8 +22,8 @@ GLMatrixStack		projectionMatrix;		// 投影矩阵
 GLFrustum			viewFrustum;			// 视景体
 GLGeometryTransform	transformPipeline;		// 几何图形变换管道
 
-GLTriangleBatch		torusBatch;             //大球
-GLTriangleBatch     sphereBatch;            //小球
+GLTriangleBatch		bigBatch;             //大球
+GLTriangleBatch     smallBatch;            //小球
 GLBatch                floorBatch;          //地板
 
 //角色帧 照相机角色帧
@@ -51,28 +51,97 @@ void SetupRC()
         floorBatch.Vertex3f(-20.0f, -0.55f, x);
     }
     floorBatch.End();
+    
+    gltMakeSphere(bigBatch, 0.4f, 40, 40);
+    gltMakeSphere(smallBatch, 0.1f, 13, 26);
+    
+    for (int i = 0; i < NUM_SPHERES; i++) {
+        GLfloat x = ((GLfloat)((rand() % 400) - 200 ) * 0.1f);
+        GLfloat z = ((GLfloat)((rand() % 400) - 200 ) * 0.1f);
+        spheres[i].SetOrigin(x, 0.0f, z);
+    }
 }
 
 //进行调用以绘制场景
 void RenderScene(void)
 {
     static GLfloat vFloorColor[] = { 0.0f, 1.0f, 0.0f, 1.0f};
+    static GLfloat vBig[] = {1,0,0,1};
+    static GLfloat vSmall[] = {0,0,1,1};
+    
+    static CStopWatch timer;
+    float bot = timer.GetElapsedSeconds() * 60;
+    
+    modelViewMatrix.PushMatrix();
+    M3DMatrix44f cCamera;
+    cameraFrame.GetCameraMatrix(cCamera);
+    modelViewMatrix.PushMatrix(cCamera);
+    
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     shaderManager.UseStockShader(GLT_SHADER_FLAT,
                                  transformPipeline.GetModelViewProjectionMatrix(),
                                  vFloorColor);
     floorBatch.Draw();
+    
+    M3DVector4f sLight = {0,15,5,1};
+    
+    modelViewMatrix.Translate(0, 0, -3);
+    modelViewMatrix.PushMatrix();
+    modelViewMatrix.Rotate(bot, 0, 1, 0);
+    shaderManager.UseStockShader(GLT_SHADER_POINT_LIGHT_DIFF,transformPipeline.GetModelViewMatrix(),transformPipeline.GetProjectionMatrix(),sLight,vBig);
+    bigBatch.Draw();
+    modelViewMatrix.PopMatrix();
+    
+    for (int i = 0; i < NUM_SPHERES; i ++) {
+        modelViewMatrix.PushMatrix();
+        modelViewMatrix.MultMatrix(spheres[i]);
+        shaderManager.UseStockShader(GLT_SHADER_POINT_LIGHT_DIFF,transformPipeline.GetModelViewMatrix(),transformPipeline.GetProjectionMatrix(),sLight,vSmall);
+        smallBatch.Draw();
+        modelViewMatrix.PopMatrix();
+    }
+    
+    modelViewMatrix.Rotate(bot * -2.5, 0, 1, 0);
+    modelViewMatrix.Translate(1.0f, 0, 0);
+    shaderManager.UseStockShader(GLT_SHADER_POINT_LIGHT_DIFF,transformPipeline.GetModelViewMatrix(),transformPipeline.GetProjectionMatrix(),sLight,vSmall);
+    smallBatch.Draw();
+    
+    
+    modelViewMatrix.PopMatrix();
+    modelViewMatrix.PopMatrix();
     glutSwapBuffers();
+    glutPostRedisplay();
 }
 
 void ChangeSize(int nWidth, int nHeight)
 {
     glViewport(0, 0, nWidth, nHeight);
     viewFrustum.SetPerspective(35.0f, float(nWidth)/float(nHeight), 1.0f, 100.0f);
-    //并将其加载到投影矩阵堆栈上
     projectionMatrix.LoadMatrix(viewFrustum.GetProjectionMatrix());
     transformPipeline.SetMatrixStacks(modelViewMatrix, projectionMatrix);
 }
+
+void SpeacialKeys(int key,int x,int y){
+    
+    float linear = 0.1f;
+    float angular = float(m3dDegToRad(5.0f));
+    
+    if (key == GLUT_KEY_UP) {
+        cameraFrame.MoveForward(linear);
+    }
+    if (key == GLUT_KEY_DOWN) {
+        cameraFrame.MoveForward(-linear);
+    }
+    
+    if (key == GLUT_KEY_LEFT) {
+        cameraFrame.RotateWorld(angular, 0, 1, 0);
+    }
+    if (key == GLUT_KEY_RIGHT) {
+        cameraFrame.RotateWorld(-angular, 0, 1, 0);
+    }
+
+}
+
+
 
 int main(int argc, char* argv[])
 {
@@ -83,6 +152,7 @@ int main(int argc, char* argv[])
     glutCreateWindow("OpenGL SphereWorld");
     glutReshapeFunc(ChangeSize);
     glutDisplayFunc(RenderScene);
+    glutSpecialFunc(SpeacialKeys);
     
     GLenum err = glewInit();
     if (GLEW_OK != err) {
